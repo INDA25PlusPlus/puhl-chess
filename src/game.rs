9900377dcb2f802b56ecaaee0dd5db339075303a
Jakob Puhl <puhl@kth.se>
2025-09-13 15:45:44 +0200

    Experimenting with checking for check, will probably remove and test another idea

--- src/game.rs
+++ src/game.rs
@@ -1,37 +1,73 @@
+use std::thread::current;
+
 use crate::board::*;
 use crate::chess_board::*;
 use crate::precompute_masks::*;
 use crate::dir::*;
 use crate::piece::*;
 
-const INDEX64: [usize; 64] = [
-    0,  1, 48,  2, 57, 49, 28,  3,
-    61, 58, 50, 42, 38, 29, 17,  4,
-    62, 55, 59, 36, 53, 51, 43, 22,
-    45, 39, 33, 30, 24, 18, 12,  5,
-    63, 47, 56, 27, 60, 41, 37, 16,
-    54, 35, 52, 21, 44, 32, 23, 11,
-    46, 26, 40, 15, 34, 20, 31, 10,
-    25, 14, 19,  9, 13,  8,  7,  6
-];
-
-// https://www.chessprogramming.org/BitScan
-fn bit_scan_forward(bb: Board) -> usize {
-    let debruijn64: Board = 0x03f79d71b4cb0a89;
-    assert!(bb != 0);
-    INDEX64[(((bb & (bb.wrapping_neg())).wrapping_mul(debruijn64)) >> 58) as usize]
-}
-
 // https://www.chessprogramming.org/Blockers_and_Beyond
 fn get_piece_moves(square: usize, mut potential_squares: Board, all_pieces: Board) -> Board {
     let mut remaining_to_check = potential_squares & all_pieces;
     while remaining_to_check != 0 {
-        let slot = bit_scan_forward(remaining_to_check);
+        let slot: usize = remaining_to_check.trailing_zeros() as usize;
         let dir = Dir::FROM_SQUARES_PAIRS[square][slot].unwrap();
         potential_squares &= !ATTACKS_MASKS.rays[slot][dir as usize];
         remaining_to_check &= remaining_to_check - 1;
     }
     potential_squares
+}
+
+fn all_squares_that_block_check(chess_board: ChessBoard, square_index: usize) -> Board {
+    let rank = rank_index(square_index);
+    let file = file_index(square_index);
+    let square = single_square_board(rank as isize, file as isize);
+
+    // Should not be called with the king
+    assert_eq!(square & chess_board.pieces[Piece::King as usize], 0);
+
+    // Remove piece square from board
+    let current_side = if chess_board.white_turn { chess_board.all_white } else { chess_board.all_black } & !square;
+    let opposite_side = if chess_board.white_turn { chess_board.all_black } else {chess_board.all_white } & !square;
+
+    // Contains the positions positions the piece can go to without its own king being in check
+    let mut current_mask = Board::MAX;
+
+    // The pieces king
+    let king_square = chess_board.pieces[Piece::King as usize] & current_side;
+    let king_index = (king_square & king_square.wrapping_neg()) as usize;
+
+    // Go through all the pieces types
+    for piece_type in [Piece::PawnWhite, Piece::PawnBlack, Piece::Knight, Piece::Bishop, Piece::Rook, Piece::Queen] {
+        // Goes through all the individual pieces on each piece types board
+        let mut piece_board: Board = chess_board.pieces[piece_type as usize] & opposite_side;
+        while piece_board != 0 {
+            // Singles out the LSB in board
+            let square = (piece_board & piece_board.wrapping_neg()) as Board;
+            let square_index = piece_board.trailing_zeros() as usize;
+            // Removes the LSB in board
+            piece_board &= piece_board - 1;
+
+            // Checks all the pieces the opposite piece attacks
+            let pieces_attacked = ATTACKS_MASKS.pieces[piece_type as usize][square_index] & current_side;
+            // Continues with another pieces if it doesn't attack the king
+            if king_square & pieces_attacked == 0 { continue };
+            // 'Ands' the mask with what positions blocks the check from this opposite piece
+            //      We 'and' it because if there are multiple pieces attacking the king, then we have to block each of those pieces
+            match piece_type {
+                Piece::Bishop | Piece::Rook | Piece::Queen => {
+                    let dir = Dir::FROM_SQUARES_PAIRS[king_index][square_index].unwrap();   // Should never return None
+                    current_mask &= (ATTACKS_MASKS.rays[king_index][dir as usize] & ATTACKS_MASKS.rays[square_index][Dir::opposite(dir) as usize]) | square;    // TODO: Precompute a segment table
+                }
+                Piece::PawnWhite | Piece::PawnBlack | Piece::Knight => {
+                    current_mask &= square;
+                }
+                _ => { unreachable!() }
+            }
+        }
+    }
+
+    current_mask
 }
 
 pub trait PieceMover {
@@ -44,24 +80,7 @@ pub trait BlockCheckMover: PieceMover {
     // Returns all moves for piece which prevents check
     // Returns all squares which prevents check
     //      OBS: if from multiple angles then you can't block it
-    fn get_moves(&self, chess_board: ChessBoard, square_index: usize) -> Board {
-        let rank = rank_index(square_index);
-        let file = file_index(square_index);
-        let square = single_cell_board(rank as isize, file as isize);
-        // Remove piece square from current setup
-        let all_other_pieces = (chess_board.all_white | chess_board.all_black) & !square;
-    
-        let current_side = if chess_board.white_turn { chess_board.all_white } else { chess_board.all_black };
-        let opposite_side = if chess_board.white_turn { chess_board.all_black } else {chess_board.all_white };
-
-        for pieace_type in ALL_PIECES {
-            get_piece_moves(square_index, ATTACKS_MASKS.pieces[pieace_type as usize][square_index], all_other_pieces);
-            match pieace_type
-        }
-        chess_board.pieaces;
-
-        todo!()
-    }
+    // For king specifically, do get_pieces_moves for every enemy piece and union them together to check, then and with the kings eight pseudo possible moves
 }
 
 pub trait SlidingMover: PieceMover {
@@ -92,4 +111,9 @@ pub trait QueenMover {
 
 pub trait KingMover {
 
+}
+
+pub trait PawnMover {
+    // (1) Get all moves which blocks a check
+    // (2) 
 }
