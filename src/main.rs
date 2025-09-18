262d3cf1219cc37c4c5cfb74214714f74c46535c
Jakob Puhl <puhl@kth.se>
2025-09-18 18:09:24 +0200

    Facade should now be useable for perft

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use chess_game::facade::*;
 
 fn main() {
-    let chess_board = ChessBoard::new(None);
+    let chess_board = PuhlChess::new(None);
     let moves = chess_board.square(Rank::new(0).unwrap(), File::new(0).unwrap()).get_moves();
-    let chess_board = moves[0].make_move();
+    // let chess_board = moves[0].make_move();
 }
